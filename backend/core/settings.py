13178878a7d3644f6b986b93bbd03860be32e3c0
Subomi194 <subomiariyo@gmail.com>
2026-05-05 18:06:53 +0100

    updated3 settings.py for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,8 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
