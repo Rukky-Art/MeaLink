44812546db74767a7859d8933f839e0dc55b97ab
Rukky-Art <akorederuqoyyah1012@gmail.com>
2026-06-07 10:39:07 +0100

    Routed the Admin's Dashboard

--- frontend/src/pages/DashboardHome.jsx
+++ frontend/src/pages/DashboardHome.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import DonorDashboard from './donorFolder/DonorDashboard';
 import PartnerDashboard from './PartnerDashboard';
+import AdminDashboard from './adminFolder/AdminDashboard';
 
 const DashboardHome = () => {
   const { user } = useSelector((state) => state.auth);
@@ -13,7 +14,7 @@ switch (user?.role) {
     case 'receiver':
       return <div className="p-8 text-center">Receiver Dashboard coming soon!</div>;
     case 'admin':
-      return <div className="p-8 text-center">Welcome, Admin.</div>;
+      return <AdminDashboard />;
     default:
       return <div className="p-8 text-center text-red-500">Unauthorized role or data error.</div>;
   }
