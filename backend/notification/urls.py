53b98e263471631f2c38e7de65c158ec51f70f7a
Subomi194 <subomiariyo@gmail.com>
2026-06-12 18:40:38 +0100

    feat: in app notifications url

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from notification.views import NotificationView, MarkAsReadView, NotificationCountView
 
 urlpatterns = [
-    path('notifications/', NotificationView.as_view(), name='notification-list'),
-    path('notifications/<int:pk>/read/', MarkAsReadView.as_view(), name='mark-as-read'),
-    path('notifications/unread-count/', NotificationCountView.as_view(), name='notification-unread-count'), 
+    path('', NotificationView.as_view(), name='notification-list'),
+    path('<int:pk>/read/', MarkAsReadView.as_view(), name='mark-as-read'),
+    path('unread-count/', NotificationCountView.as_view(), name='notification-unread-count'), 
 ]
